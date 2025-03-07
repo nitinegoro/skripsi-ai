7415fb864a8ac6034da85385a2e3065e53d8e94e
Bookmu Id <bookmu.id@gmail.com>
2025-03-08 04:52:33 +0700

    add gh-page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/skripsi-ai/' : '',
+  basePath: isProd ? '/skripsi-ai' : '',
+  output: 'export'
+};
 
 module.exports = nextConfig
